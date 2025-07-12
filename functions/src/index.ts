9f42744feb885f59178b49f4cc13cc05a296c1a9
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2025-07-12 10:21:53 +0200

    Create basic structure

---
+++ functions/src/index.ts
@@ -0,0 +1,7 @@
+import { onRequest } from "firebase-functions/v2/https";
+import {logger} from "firebase-functions/v2";
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
